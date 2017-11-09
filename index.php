270af64f885a307080f7512d4660dc705357a095
TomasStrolys <33262525+TomasStrolys@users.noreply.github.com>
2017-11-09 19:00:39 +0200

    Table added

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Bakery</title>
+</head>
+<body>
+<table>
+	<thead>
+		<tr>
+			<td>Pavadinimas</td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>Aguonine</td>
+		</tr>
+		<tr>
+			<td>Varškės</td>
+		</tr>
+		<tr>
+			<td>Duona_balta</td>
+		</tr>
+		<tr>
+			<td>Duona_juoda</td>
+		</tr>
+		<tr>
+			<td>Džemas</td>
+		</tr>
+	</tbody>
+
+
+</table>
+
+
+</body>
+</html>
